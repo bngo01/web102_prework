6992e8e13026ebb4e9ef553f0b97b1b3a6824e72
Bao Ngo <b.ngo3048@gmail.com>
2023-01-14 10:45:39 -0500

    completed challenege 7, working on additional improvements

--- index.js
+++ index.js
@@ -172,7 +172,15 @@ const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
 });
 
 // use destructuring and the spread operator to grab the first and second games
+let [firstGame, secondGame] = sortedGames 
 
 // create a new element to hold the name of the top pledge game, then append it to the correct element
+let firstGameName = document.createElement("p")
+firstGameName.innerHTML = firstGame.name
+firstGameContainer.appendChild(firstGameName)
+
+let secondGameName = document.createElement("p")
+secondGameName.innerHTML = secondGame.name
+secondGameContainer.appendChild(secondGameName)
 
 // do the same for the runner up item
